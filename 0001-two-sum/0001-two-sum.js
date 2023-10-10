883beb51ba7b2a8981d0122c6618c4c9be58abb5
shin.heehyun <96459468+hxxhyun@users.noreply.github.com>
2023-10-10 10:23:00 +0900

    Time: 142 ms (11.98%), Space: 42.3 MB (70.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,12 +7,9 @@ var twoSum = function(nums, target) {
     let result = [];
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j <nums.length; j++) {
-            if (i !== j && i < j) {
-                if (nums[i] + nums[j] === target) {
+            if (i < j && nums[i] + nums[j] === target) {
                     result.push(i);
                     result.push(j);
-                    break;
-                }
             }
         }
     }
